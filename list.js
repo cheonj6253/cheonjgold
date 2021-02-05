17c6bfe74f57a11625f9ed4be166bd2cc9a77415
cheonj6253 <78545668+cheonj6253@users.noreply.github.com>
2021-02-05 23:54:50 +0900

    Add files via upload
    up3

---
+++ list.js
@@ -0,0 +1,10 @@
+function searching(){
+    var item=document.querySelector('#item');
+
+    if(item.value=="260"){
+        location.href="G260.HTML";
+    }
+    else{
+        alert("등록되지않았거나 없는 제품입니다.")
+    }
+}
